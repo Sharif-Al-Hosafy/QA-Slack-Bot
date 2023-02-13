de626ae2717600559bf868d6f5ee9793ac848e8e
Sharif Al Hosafy <sci.sharifismail2018@alexu.edu.eg>
2023-02-13 14:38:17 +0200

    bot-saying-hello

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 const { App } = require("@slack/bolt");
 
-// Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
+
+app.message("hello", async ({ message, say }) => {
+  await say(`Hey there <@${message.user}>!`);
 });
 
 (async () => {
-  // Start your app
   await app.start(process.env.PORT || 3000);
 
   console.log("⚡️ CamQA app is running!");
